50dd45e189e539150c38be70a56bf98c39f2cc70
Chloe Aribo <chloearibo92@gmail.com>
2020-05-01 00:46:52 -0700

    edited styling

--- Components/Forum/Forum.jsx
+++ Components/Forum/Forum.jsx
@@ -1,23 +1,22 @@
 import React, {Component} from 'react';
 import Header from '../Header.jsx';
 import Footer from '../Footer.jsx';
-import forumStyles from './forumStyles.scss';
 import ForumContainer from './ForumContainer.jsx';
+import forumStyles from '../Forum/forumStyles.scss';
 import { withStyles, Typography, CardContent, Card, TextField, Button } from '@material-ui/core';
 
 const styles = (theme) => ({
-    root: {
-      width: 900,
-      height: 600,
+    comments: {
+      height: 425,
       backgroundColor: 'transparent',
-      boxShadow: "10px 10px 10px 10px grey"
+      overflow: "auto",
     },
     form: {
         '& .MuiTextField-root': {
           margin: theme.spacing(1),
-          width: '19.2ch',
-          backgroundColor: 'transparent',
+          width: '40ch',
         },
+        backgroundColor: 'transparent',
       },
   });
 
@@ -31,8 +30,8 @@ class Forum extends Component {
         };
         this.getComments = this.getComments.bind(this);
         this.postText = this.postText.bind(this);
-        this.handleCommentChange = this.handleCommentChange.bind(this)
-        this.handleTitleChange = this.handleTitleChange.bind(this)
+        this.handleCommentChange = this.handleCommentChange.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this);
     };
 
     componentDidMount () {
@@ -42,14 +41,14 @@ class Forum extends Component {
     handleCommentChange (event) {
         this.setState({
             comment: event.target.value
-        })
-    }
+        });
+    };
 
     handleTitleChange () {
         this.setState({
             title: event.target.value
-        })
-    }
+        });
+    };
 
     /* GET all comments from DB and pass them as props to the ForumContainer.jsx */
     getComments () {
@@ -74,11 +73,8 @@ class Forum extends Component {
 
     /* Send POST request to BD and append comment to document (invoke getComments) */
     postText () {
-        console.log('hi in post text')
-        let newText = this.state.comment//document.getElementById('inputTitle').value;
-        let newTitle = this.state.title //document.getElementById('inputText').value;
-        console.log('in post text', newText, newTitle)
-
+        let newText = this.state.comment;
+        let newTitle = this.state.title;
         fetch('/api/forum/:id', {
             method: 'POST', 
             headers: {'Content-Type' : 'application/json'},
@@ -90,62 +86,64 @@ class Forum extends Component {
             })
         })
         .then(() => this.getComments())
-    }
+    };
 
 
     render () {
         let returnVal = [];
         this.state.all.length > 0 ? returnVal.push(<ForumContainer props={this.state.all} func={this.getComments} />) : null;
-        let { classes } = this.props
-        let title = this.state.title
-        let comment = this.state.comment
+        let { classes } = this.props;
+        let title = this.state.title;
+        let comment = this.state.comment;
         return (
             <div className='forumPage'>
                 <Header/>
                 <div className='commentCard'>
-                    <Card className={classes.root}>
-                        <CardContent id='comments' >
-                            <div className='commentCardTitle'>
-                                <Typography variant='h6'>Post A Comment With The KYODIE Community</Typography>
-                            </div>
-                            <div >
-                                {returnVal}
+                    <div className='commentCardTitle'>
+                        <Typography variant='h6'>Post A Comment With The KYODIE Community</Typography>
+                    </div>
+                    <Card className={classes.comments}>
+                        <CardContent >
+                            {returnVal}
+                        </CardContent>
+
+                    </Card>
+                    <Card className={classes.form}>
+                        <CardContent> 
+                            <div>
+                                <form>
+                                    <TextField 
+                                        id='outlined-basic' 
+                                        value={title} 
+                                        required
+                                        label='Title Of Post'
+                                        className='title'
+                                        variant='outlined'
+                                        onChange={this.handleTitleChange} />
+                                    <TextField
+                                        id="outlined-multiline-static"
+                                        value={comment}
+                                        required
+                                        label='Comment'
+                                        className='comment'
+                                        variant='outlined'
+                                        onChange={this.handleCommentChange} />
+                                    <Button 
+                                        size='medium'
+                                        p={5}
+                                        variant="outlined" 
+                                        color="inherit" 
+                                        onClick={this.postText} >
+                                            Submit
+                                    </Button>
+                                </form>
                             </div>
                         </CardContent>
                     </Card>
                 </div>
-                <div className={classes.form}>
-                    <form>
-                        <TextField 
-                            id='outlined-basic' 
-                            value={title} 
-                            required
-                            label='Title Of Post'
-                            className='title'
-                            variant='outlined'
-                            onChange={this.handleTitleChange} /> <br></br>
-                        <TextField
-                            id="outlined-multiline-static"
-                            value={comment}
-                            required
-                            label='Comment'
-                            className='comment'
-                            variant='outlined'
-                            onChange={this.handleCommentChange} /> <br></br>
-                        <Button 
-                            size='medium'
-                            p={5}
-                            variant="outlined" 
-                            color="inherit" 
-                            onClick={this.postText} >
-                                Submit
-                        </Button>
-                        {/* <button onClick={this.postText}>Submit</button> */}
-                    </form>
-                </div>
                 <Footer/>
             </div>
-        )
-    }
-}
+        );
+    };
+};
 export default withStyles(styles)(Forum);
